39ed31bf8952b024cf1b78767e78150c988c09e8
Guillaume Dore <corwin@poussif.eu>
2019-02-01 11:15:27 +0100

    finally a working supervisorctl status beat

--- beater/supervisorctlbeat.go
+++ beater/supervisorctlbeat.go
@@ -1,78 +1,125 @@
 package beater
 
 import (
-	"fmt"
-	"time"
+    "fmt"
+    "time"
 
-        "os/exec"
+    "os/exec"
+    "strings"
 
-	"github.com/elastic/beats/libbeat/beat"
-	"github.com/elastic/beats/libbeat/common"
-	"github.com/elastic/beats/libbeat/logp"
+    "github.com/elastic/beats/libbeat/beat"
+    "github.com/elastic/beats/libbeat/common"
+    "github.com/elastic/beats/libbeat/logp"
 
-	"github.com/Corwind/supervisorctlbeat/config"
+    "github.com/Corwind/supervisorctlbeat/config"
 )
 
 // Supervisorctlbeat configuration.
 type Supervisorctlbeat struct {
-	done   chan struct{}
-	config config.Config
-	client beat.Client
+    done   chan struct{}
+    config config.Config
+    client beat.Client
 }
 
 // New creates an instance of supervisorctlbeat.
 func New(b *beat.Beat, cfg *common.Config) (beat.Beater, error) {
-	c := config.DefaultConfig
-	if err := cfg.Unpack(&c); err != nil {
-		return nil, fmt.Errorf("Error reading config file: %v", err)
-	}
+    c := config.DefaultConfig
+    if err := cfg.Unpack(&c); err != nil {
+        return nil, fmt.Errorf("Error reading config file: %v", err)
+    }
 
-	bt := &Supervisorctlbeat{
-		done:   make(chan struct{}),
-		config: c,
-	}
-	return bt, nil
+    bt := &Supervisorctlbeat{
+        done:   make(chan struct{}),
+        config: c,
+    }
+    return bt, nil
 }
 
 // Run starts supervisorctlbeat.
 func (bt *Supervisorctlbeat) Run(b *beat.Beat) error {
-	logp.Info("supervisorctlbeat is running! Hit CTRL-C to stop it.")
+    logp.Info("supervisorctlbeat is running! Hit CTRL-C to stop it.")
 
-	var err error
-	bt.client, err = b.Publisher.Connect()
-	if err != nil {
-		return err
-	}
+    var err error
+    bt.client, err = b.Publisher.Connect()
+    if err != nil {
+        return err
+    }
 
-        var fields = common.MapStr{
-                "type":    b.Info.Name,
+    var fields = common.MapStr{
+        "type":    b.Info.Name,
+    }
+
+
+    //var out bytes.Buffer
+    logp.Info("Before vars")
+
+
+    ticker := time.NewTicker(bt.config.Period)
+    for {
+        select {
+        case <-bt.done:
+            return nil
+        case <-ticker.C:
         }
 
-        var output []byte
+        logp.Info("Starting command")
+        output_, err := exec.Command("supervisorctl", "-c", bt.config.SupervisordConfig, "status").Output()
 
-	ticker := time.NewTicker(bt.config.Period)
-	counter := 1
-	for {
-		select {
-		case <-bt.done:
-			return nil
-		case <-ticker.C:
-		}
+        if err != nil {
+            logp.Err("An error occured while executing command: %v", err)
+            return err
+        }
 
-                output, err = exec.Command("supervisorctl", "-c", 
-                                           bt.config.SupervisorConf, "status").Output()
+        status := common.MapStr {
+            "RUNNING": 1,
+            "FATAL": -1,
+            "STOPPED": -2,
+        }
 
-		event := beat.Event{
-			Timestamp: time.Now(),
-                        Fields: fields,
-		}
-		bt.client.Publish(event)
-		logp.Info("Event sent")
-	}
+        var services = common.MapStr{}
+
+        output := strings.Split(string(output_), "\n")
+
+
+        for _, element := range output {
+
+            if len(element) == 0 { break }
+
+            var split_element = strings.Fields(element)
+            var state = common.MapStr{}
+
+            state.Put("state", status[split_element[1]])
+            state.Put("state_str", split_element[1])
+
+            if (split_element[1] == "STOPPED" ||
+                split_element[1] == "FATAL") {
+
+                state.Put("reason", strings.Join(split_element[2:], " "))
+
+            } else if (split_element[1] == "RUNNING") {
+
+                pid := split_element[3]
+                state.Put("pid", pid[:len(pid) - 1])
+                state.Put("uptime", strings.Join(split_element[5:], " "))
+
+            }
+
+            services.Put(split_element[0], state)
+        }
+
+        fields.Put("services", services)
+
+        event := beat.Event{
+            Timestamp: time.Now(),
+            Fields: fields,
+        }
+        bt.client.Publish(event)
+        logp.Info("Event sent")
+    }
 }
 
 // Stop stops supervisorctlbeat.
 func (bt *Supervisorctlbeat) Stop() {
-	bt.client.Close()
-	close(bt.done)
+    bt.client.Close()
+    close(bt.done)
 }
